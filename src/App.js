8ebc910b041296055544d1a524b8cb4a588baa7e
NZY1337 <mandreicosmin@yahoo.com>
2019-09-25 21:48:43 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+// this is like our homepage
+class App extends Component {
+  render() {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {Route, Link, BrowserRouter as Router} from 'react-router-dom';
+import App from './App';
+import Users from './components/Users';
+import Contact from './components/Conctact';
+
+
+const routing = (
+    <Router>
+      <div> 
+          <Route  path="/" component={App} /> {/*  this is like my homepage, without any path in it */}
+          <Route path="/users" component={Users} />
+          <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
+  );
+  
+ReactDOM.render(routing, document.getElementById('root'));
+  
+
+
+
