8acb247760c44e03d8745b6c165230f6412bab06
NZY1337 <mandreicosmin@yahoo.com>
2019-10-01 22:10:59 +0300

    changed the carousel dots

--- src/Home.js
+++ src/Home.js
@@ -8,23 +8,23 @@ class Homepage extends Component {
   render() {
     return (
         <div>
-                  <Carousel options={{fullWidth: true,indicators: true, numVisible:true}}
-                 images={[
-                    'https://images.pexels.com/photos/2818756/pexels-photo-2818756.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-                    'https://images.pexels.com/photos/1115804/pexels-photo-1115804.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-                    'https://images.pexels.com/photos/2825033/pexels-photo-2825033.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-                ]} />
+            <Carousel options={{fullWidth: true,indicators: true, numVisible:true}}
+                images={[
+                'https://images.pexels.com/photos/2818756/pexels-photo-2818756.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+                'https://images.pexels.com/photos/2805384/pexels-photo-2805384.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+                'https://images.pexels.com/photos/2825033/pexels-photo-2825033.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+            ]} />
 
             <div className="container">
-                    <h3>Contact</h3>
-                    <hr/>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Exercitationem commodi omnis perspiciatis minus adipisci earum tempora vero aut repellendus totam! Omnis repellat non nesciunt et, facilis aliquam quos ipsum nihil.</p>
-                    <Modal header="Modal Header" topSheet trigger={<Button>Open<Icon right>send</Icon></Button>}>
-                        asd
-                    </Modal>
-                </div>
+                <h3>Contact</h3>
+                <hr/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Exercitationem commodi omnis perspiciatis minus adipisci earum tempora vero aut repellendus totam! Omnis repellat non nesciunt et, facilis aliquam quos ipsum nihil.</p>
+                <Modal header="Modal Header" topSheet trigger={<Button>Open<Icon right>send</Icon></Button>}>
+                    asd
+                </Modal>
             </div>
+        </div>
     );
   }
 }
